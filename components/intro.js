0eda23abaafba50af53db778c6eb96a010b7e1ce
Joana Tavares <joanalt@ua.pt>
2022-10-29 09:47:46 +0100

    components

--- components/intro.js
+++ components/intro.js
@@ -6,7 +6,6 @@ export default function Intro() {
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog.
       </h1>
-      <h5>Valida que o teu site está a ser actualizado sempre que faças um commit para “master” ou “main”</h5>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         A statically generated blog example using{" "}
         <a
